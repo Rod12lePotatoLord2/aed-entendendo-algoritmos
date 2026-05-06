91967dfeb87db72112f3d9d095b61237646c2f2e
Rodrigo Lima <rodrigolimabarbosa.12@gmail.com>
2026-05-06 18:54:50 -0300

    Implementado reverse_array in-place

--- src/reverse_array.py
+++ src/reverse_array.py
@@ -2,4 +2,18 @@ from src.my_array import MyArray
 
 
 def reverse_array(array: MyArray) -> MyArray:
-    raise NotImplementedError
+    n = len(array)
+    inicio = 0
+    fim = n - 1
+
+    while inicio < fim:
+        valor_inicio = array[inicio]
+        valor_fim = array[fim]
+
+        array[inicio] = valor_fim
+        array[fim] = valor_inicio
+
+        inicio += 1
+        fim -= 1
+    
+    return array
