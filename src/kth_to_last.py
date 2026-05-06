42ebc4266185ac21030c43315046dd60859d3bb2
Rodrigo Lima <rodrigolimabarbosa.12@gmail.com>
2026-05-06 19:34:07 -0300

    Exercícios 04, 05 e 06: Inversão, K-ésimo elemento e Detecção de Ciclo em Listas Encadeadas

--- src/kth_to_last.py
+++ src/kth_to_last.py
@@ -1,5 +1,20 @@
 from src.my_node import MyNode
 
 
-def kth_to_last(head: MyNode, k: int) -> int:
-    raise NotImplementedError
+def kth_to_last(head: MyNode, k: int):
+    if head is None or k <= 0:
+        return -1
+        
+    ponteiro_frente = head
+    ponteiro_tras = head
+    
+    for _ in range(k):
+        if ponteiro_frente is None:
+            return -1
+        ponteiro_frente = ponteiro_frente.next
+        
+    while ponteiro_frente is not None:
+        ponteiro_frente = ponteiro_frente.next
+        ponteiro_tras = ponteiro_tras.next
+        
+    return ponteiro_tras.value
--- src/reverse_linked_list.py
+++ src/reverse_linked_list.py
@@ -2,4 +2,11 @@ from src.my_node import MyNode
 
 
 def reverse_linked_list(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    anterior = None
+    atual = head
+    while atual is not None:
+        proximo_no = atual.next
+        atual.next = anterior
+        anterior = atual
+        atual = proximo_no
+    return anterior
