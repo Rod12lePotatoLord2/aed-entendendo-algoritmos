1ec8624514d7aa81003c7b6a77daf4815cb9d5e6
Rodrigo Lima <rodrigolimabarbosa.12@gmail.com>
2026-05-06 20:11:28 -0300

    Fix: removendo uso de set em remove_duplicates para seguir restrições

--- src/remove_duplicates.py
+++ src/remove_duplicates.py
@@ -4,15 +4,14 @@ def remove_duplicates(head: MyNode) -> MyNode:
     if head is None:
         return None
     
-    valores_vistos = set()
     atual = head
-    valores_vistos.add(atual.value)
-    
-    while atual.next is not None:
-        if atual.next.value in valores_vistos:
-            atual.next = atual.next.next
-        else:
-            valores_vistos.add(atual.next.value)
-            atual = atual.next
+    while atual is not None:
+        corredor = atual
+        while corredor.next is not None:
+            if corredor.next.value == atual.value:
+                corredor.next = corredor.next.next
+            else:
+                corredor = corredor.next
+        atual = atual.next
             
     return head
